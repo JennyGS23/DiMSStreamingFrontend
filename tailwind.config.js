39b975003a4c2fa3032b9afc398829b51f13fd9e
JENNIFER GONZALEZ SOLIS <gonzalezsolisjennifer@gmail.com>
2024-09-13 22:13:14 -0600

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",  // Esto buscará todos los archivos HTML en la raíz
+    "./**/*.html" // Esto buscará archivos HTML en cualquier subcarpeta
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'color-components': '#0CBABA',
+        'color-bar-nav': '#9984D4',
+        'color-principal': '#000505',
+        'color-secondary': '#282B28',
+      },
+      backgroundImage: {
+        'fond-recommends': "url('/img/recomendations.png')",
+      }
+    },
+  },
+  plugins: [],
+}
